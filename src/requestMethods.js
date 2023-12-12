7aba4092322c043184349c773601f85265e0e639
Nhan Hao <truongnhanhao107@gmail..com>
2023-12-12 20:02:01 +0700

    3 commit

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = "http://localhost:5000/api/";
 const BASE_URL_TRAM = "http://ctu-oda-gateway.site/";
 const BASE_URL_TRAM2 = "http://13.229.123.104";
-const BASE_URL_TRAM3 = "http://3.0.21.185";
+const BASE_URL_TRAM3 = "https://3.0.21.185";
 export const publicRequestTram = axios.create({
   baseURL: BASE_URL_TRAM3,
   headers: {
